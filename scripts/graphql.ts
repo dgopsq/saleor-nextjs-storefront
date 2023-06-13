c049953926243076224fee08a87c211022fcaac7
Diego Pasquali <imdiegopasquali@gmail.com>
2023-06-13 17:40:42 +0200

    Add graphql config

---
+++ scripts/graphql.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "https://orologiomalvagio.eu.saleor.cloud/graphql/",
+  documents: "../src/**/*.tsx",
+  generates: {
+    "../src/gql/": {
+      preset: "client",
+      plugins: [],
+    },
+  },
+};
+
+export default config;
