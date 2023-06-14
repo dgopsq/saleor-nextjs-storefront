116541fc8750f7c5322c5de7eb96640bcc0d4ff9
Diego Pasquali <imdiegopasquali@gmail.com>
2023-06-14 09:07:43 +0200

    Add basic product filters

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import { GetHomepageProductsDocument } from "@/__generated__/graphql";
+import {
+  GetProductsDocument,
+  OrderDirection,
+  ProductOrderField,
+} from "@/__generated__/graphql";
 import { SingleProduct } from "@/components/products/SingleProduct";
 import { useQuery } from "@apollo/client";
 
 export const Products: React.FC = () => {
-  const { loading, error, data } = useQuery(GetHomepageProductsDocument, {
-    variables: { first: 10 },
+  const { loading, error, data } = useQuery(GetProductsDocument, {
+    variables: {
+      first: 10,
+      filters: { categories: ["Q2F0ZWdvcnk6Mjc="] },
+      sortBy: { field: ProductOrderField.Price, direction: OrderDirection.Asc },
+    },
   });
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
 
   return (
-    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8">
       {data?.products?.edges?.map(({ node: product }) => (
         <SingleProduct
           key={product.id}
