17277394fbfed14dc3f5df5761bc1d70f1fc6b7d
Diego Pasquali <imdiegopasquali@gmail.com>
2023-06-14 17:22:37 +0200

    Use products page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { GetProductsDocument } from "@/__generated__/graphql";
-import { Products } from "@/components/products/Products";
-import { getAllProductsVariables } from "@/components/products/data";
-import { getApolloClient } from "@/misc/apollo";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-  const client = getApolloClient();
-
-  const res = await client.query({
-    query: GetProductsDocument,
-    variables: getAllProductsVariables(),
-  });
-
-  return <Products prefetchedData={res.data} />;
+  redirect("/products");
 }
---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+import { GetProductsDocument } from "@/__generated__/graphql";
+import { Products } from "@/components/products/Products";
+import { getAllProductsVariables } from "@/components/products/data";
+import { getApolloClient } from "@/misc/apollo";
+
+export default async function ProductsPage() {
+  const client = getApolloClient();
+
+  const res = await client.query({
+    query: GetProductsDocument,
+    variables: getAllProductsVariables(),
+  });
+
+  return <Products prefetchedData={res.data} />;
+}
