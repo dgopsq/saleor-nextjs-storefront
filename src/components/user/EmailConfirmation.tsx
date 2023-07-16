c0f2f23d90266ff4a435f56db892e55fb0a95927
Diego Pasquali <imdiegopasquali@gmail.com>
2023-07-16 19:38:47 +0200

    Fix email confirmation

--- src/components/user/EmailConfirmation.tsx
+++ src/components/user/EmailConfirmation.tsx
@@ -3,6 +3,7 @@
 import { UserConfirmEmailChangeDocument } from "@/__generated__/graphql";
 import { errorToast, successToast } from "@/components/core/Notifications";
 import { logger } from "@/misc/logger";
+import { useAuthToken } from "@/misc/states/authTokenStore";
 import { useMutation } from "@apollo/client";
 import { useEffect } from "react";
 
@@ -11,15 +12,16 @@ import { useEffect } from "react";
  */
 export const EmailConfirmation: React.FC = () => {
   const [confirmEmail, { data }] = useMutation(UserConfirmEmailChangeDocument);
+  const authToken = useAuthToken();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const token = queryParams.get("token");
 
-    if (!token) return;
+    if (!token || !authToken) return;
 
     confirmEmail({ variables: { token }, errorPolicy: "ignore" });
-  }, [confirmEmail]);
+  }, [confirmEmail, authToken]);
 
   useEffect(() => {
     if (data?.confirmEmailChange?.user)
