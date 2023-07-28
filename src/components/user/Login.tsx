c3e4738acc3e8ed5d2099193dac34d2c05ab4061
Diego Pasquali <imdiegopasquali@gmail.com>
2023-07-28 15:56:01 +0200

    Attach existent cart to the logged in user

--- src/components/user/Login.tsx
+++ src/components/user/Login.tsx
@@ -2,12 +2,14 @@
 
 import { getFragmentData } from "@/__generated__";
 import {
+  CheckoutAttachDocument,
   CreateTokenDocument,
   GenericUserFragmentDoc,
 } from "@/__generated__/graphql";
 import { ErrorAlert, SuccessAlert } from "@/components/core/Alert";
 import { LoginForm } from "@/components/core/LoginForm";
 import { publicConfig } from "@/misc/config";
+import { useCheckoutInfo } from "@/misc/hooks/useCheckoutInfo";
 import { logger } from "@/misc/logger";
 import { useMutation } from "@apollo/client";
 import Cookies from "js-cookie";
@@ -21,48 +23,72 @@ import { P, match } from "ts-pattern";
 export const Login: React.FC = () => {
   const [createAccount, { loading, error, data }] =
     useMutation(CreateTokenDocument);
+  const [checkoutAttach] = useMutation(CheckoutAttachDocument);
+  const checkout = useCheckoutInfo();
 
   const handleSubmit = useCallback(
-    (values: LoginForm) => {
+    async (values: LoginForm) => {
       logger.debug("Executing user authentication.");
 
-      createAccount({
+      const createAccountRes = await createAccount({
         variables: {
           email: values.email,
           password: values.password,
         },
-        onCompleted: (data) => {
-          logger.debug("Token creaded successfully, user authenticated.");
-
-          const maybeToken = data.tokenCreate?.token ?? null;
-          const maybeRefreshToken = data.tokenCreate?.refreshToken ?? null;
-          const maybeUserFragment =
-            getFragmentData(GenericUserFragmentDoc, data.tokenCreate?.user) ??
-            null;
-          const maybeCheckoutId: string | null =
-            maybeUserFragment?.checkouts?.edges[0]?.node.id ?? null;
-
-          if (maybeToken && maybeRefreshToken) {
-            logger.debug("Auth and Refresh Token found, creating cookies.");
-
-            Cookies.set(publicConfig.userTokenStorageKey, maybeToken);
-            Cookies.set(
-              publicConfig.userRefreshTokenStorageKey,
-              maybeRefreshToken
-            );
-          }
-
-          if (maybeCheckoutId) {
-            logger.debug("Checkout Token found, creating the cookie.");
-
-            Cookies.set(publicConfig.checkoutIdStorageKey, maybeCheckoutId);
-          }
-
-          window.location.href = "/account";
-        },
       });
+
+      const createAccountdata = createAccountRes.data ?? null;
+
+      if (!createAccountdata) {
+        logger.error(
+          "Error while creating the account",
+          createAccountRes.errors
+        );
+        return;
+      }
+
+      logger.debug("Token creaded successfully, user authenticated.");
+
+      const maybeToken = createAccountdata.tokenCreate?.token ?? null;
+      const maybeRefreshToken =
+        createAccountdata.tokenCreate?.refreshToken ?? null;
+      const maybeUserFragment =
+        getFragmentData(
+          GenericUserFragmentDoc,
+          createAccountdata.tokenCreate?.user
+        ) ?? null;
+      const maybeUserLastCheckoutId: string | null =
+        maybeUserFragment?.checkouts?.edges[0]?.node.id ?? null;
+
+      if (maybeToken && maybeRefreshToken) {
+        logger.debug("Auth and Refresh Token found, creating cookies.");
+
+        Cookies.set(publicConfig.userTokenStorageKey, maybeToken);
+        Cookies.set(publicConfig.userRefreshTokenStorageKey, maybeRefreshToken);
+      }
+
+      if (checkout.data?.lines?.length) {
+        logger.debug(
+          "There are items in the current checkout, assigning to user."
+        );
+
+        // FIXME: What happens if this fails?
+        // We should probably set the `maybeUserLastCheckoutId`
+        // as the new checkout in that specific case.
+        checkoutAttach({
+          variables: {
+            checkoutId: checkout.data.id,
+          },
+        });
+      } else if (maybeUserLastCheckoutId) {
+        logger.debug("Checkout Token found, creating the cookie.");
+
+        Cookies.set(publicConfig.checkoutIdStorageKey, maybeUserLastCheckoutId);
+      }
+
+      window.location.href = "/account";
     },
-    [createAccount]
+    [createAccount, checkout, checkoutAttach]
   );
 
   const signupErrors = data?.tokenCreate?.errors ?? [];
