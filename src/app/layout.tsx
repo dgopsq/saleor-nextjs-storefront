e479b59a9ac10d3e73a3427fb17e64a44a17c89e
Diego Pasquali <imdiegopasquali@gmail.com>
2023-08-02 20:56:24 +0200

    Fix horizontal space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default async function Layout({
 
           <Navbar categories={parsedCategories} />
 
-          <main className="flex flex-col items-center justify-between mt-12 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-32">
+          <main className="flex flex-col items-center justify-between mt-12 mx-auto max-w-7xl px-4 pb-32">
             {children}
           </main>
 
