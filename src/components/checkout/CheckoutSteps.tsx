9dc87a40f76ae3af3e75d4ca4179681a0c65b491
Diego Pasquali <imdiegopasquali@gmail.com>
2023-08-03 10:23:26 +0200

    Mobile checkout steps

--- src/components/checkout/CheckoutSteps.tsx
+++ src/components/checkout/CheckoutSteps.tsx
@@ -32,40 +32,82 @@ export const CheckoutSteps: React.FC<Props> = ({ currentStep }) => {
   );
 
   return (
-    <nav>
-      <ol role="list" className="space-y-4 md:flex md:space-x-8 md:space-y-0">
-        {steps.map((step, index) => (
-          <li key={step.name} className="md:flex-1">
-            {currentStep > index ? (
-              <Link href={step.route}>
-                <span className="group flex flex-col border-l-4 border-indigo-600 py-2 pl-4 hover:border-indigo-800 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
-                  <span className="text-sm font-medium text-indigo-600 group-hover:text-indigo-800">
+    <>
+      {/* Desktop */}
+      <nav className="hidden md:block">
+        <ol role="list" className="space-y-4 md:space-x-8 md:space-y-0 md:flex">
+          {steps.map((step, index) => (
+            <li key={step.name} className="md:flex-1">
+              {currentStep > index ? (
+                <Link href={step.route}>
+                  <span className="group flex flex-col border-l-4 border-indigo-600 py-2 pl-4 hover:border-indigo-800 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
+                    <span className="text-sm font-medium text-indigo-600 group-hover:text-indigo-800">
+                      {formatStepIndex(index)}
+                    </span>
+                    <span className="text-sm font-medium">{step.name}</span>
+                  </span>
+                </Link>
+              ) : currentStep === index ? (
+                <span
+                  className="flex flex-col border-l-4 border-indigo-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+                  aria-current="step"
+                >
+                  <span className="text-sm font-medium text-indigo-600">
                     {formatStepIndex(index)}
                   </span>
                   <span className="text-sm font-medium">{step.name}</span>
                 </span>
-              </Link>
-            ) : currentStep === index ? (
-              <span
-                className="flex flex-col border-l-4 border-indigo-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
-                aria-current="step"
-              >
-                <span className="text-sm font-medium text-indigo-600">
-                  {formatStepIndex(index)}
+              ) : (
+                <span className="group flex flex-col border-l-4 border-gray-200 py-2 pl-4 hover:border-gray-300 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                    {formatStepIndex(index)}
+                  </span>
+                  <span className="text-sm font-medium">{step.name}</span>
                 </span>
-                <span className="text-sm font-medium">{step.name}</span>
-              </span>
-            ) : (
-              <span className="group flex flex-col border-l-4 border-gray-200 py-2 pl-4 hover:border-gray-300 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
-                <span className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
-                  {formatStepIndex(index)}
+              )}
+            </li>
+          ))}
+        </ol>
+      </nav>
+
+      {/* Mobile */}
+      <nav
+        className="flex items-center justify-center md:hidden"
+        aria-label="Progress"
+      >
+        <ol role="list" className="flex items-center space-x-5">
+          {steps.map((step, index) => (
+            <li key={step.name}>
+              {currentStep > index ? (
+                <Link
+                  href={step.route}
+                  className="block h-2.5 w-2.5 rounded-full bg-indigo-600 hover:bg-indigo-900"
+                >
+                  <span className="sr-only">{step.name}</span>
+                </Link>
+              ) : currentStep === index ? (
+                <span className="relative flex items-center justify-center">
+                  <span
+                    className="absolute flex h-5 w-5 p-px"
+                    aria-hidden="true"
+                  >
+                    <span className="h-full w-full rounded-full bg-indigo-200" />
+                  </span>
+                  <span
+                    className="relative block h-2.5 w-2.5 rounded-full bg-indigo-600"
+                    aria-hidden="true"
+                  />
+                  <span className="sr-only">{step.name}</span>
                 </span>
-                <span className="text-sm font-medium">{step.name}</span>
-              </span>
-            )}
-          </li>
-        ))}
-      </ol>
-    </nav>
+              ) : (
+                <span className="block h-2.5 w-2.5 rounded-full bg-gray-200 hover:bg-gray-400">
+                  <span className="sr-only">{step.name}</span>
+                </span>
+              )}
+            </li>
+          ))}
+        </ol>
+      </nav>
+    </>
   );
 };
