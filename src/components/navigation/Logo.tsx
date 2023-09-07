665110abe9c6b8410b32862256683b451016a6ef
Diego Pasquali <imdiegopasquali@gmail.com>
2023-09-07 11:18:08 +0200

    Add agnostic logo

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,18 +1,13 @@
 import { Link } from "@/components/core/Link";
 import { homeRoute } from "@/misc/navigation";
-import Image from "next/image";
 
 /**
  * The Logo component.
  */
 export const Logo: React.FC = () => (
-  <Link href={homeRoute}>
-    <Image
-      src="https://tailwindui.com/img/logos/mark.svg"
-      alt=""
-      className="h-8 w-auto"
-      width={100}
-      height={100}
-    />
+  <Link href={homeRoute} className="hover:no-underline">
+    <span className="inline-block px-2 py-1 text-xl uppercase font-black text-primary-600 rounded-lg border-4 border-primary-600">
+      l o g o
+    </span>
   </Link>
 );
